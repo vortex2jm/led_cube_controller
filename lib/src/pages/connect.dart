6fad93479bc13601f17260f99b2457acb43b9c9e
vortex2jm <joaopaulomoura5328@gmail.com>
2023-04-05 16:20:24 -0300

    :recycle: Adding button to connect page

--- lib/src/pages/connect.dart
+++ lib/src/pages/connect.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:led_cube_controller/src/components/button.dart';
 import '../components/input_field.dart';
 
 class Connect extends StatelessWidget {
@@ -9,8 +10,17 @@ class Connect extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: InputField(),
-      ),
+        child: SizedBox(
+          height: 200, 
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const <Widget>[
+              InputField(width: 400, height: 50,),
+              ConnectButton(b_text: "Connect", width: 200, height: 50),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
