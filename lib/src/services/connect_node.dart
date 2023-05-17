0b5828673f46f7f361249ca35998517ca2af2f52
vortex2jm <joaopaulomoura5328@gmail.com>
2023-05-17 12:17:04 -0300

    :sparkles: Adding testing functions

--- lib/src/services/connect_node.dart
+++ lib/src/services/connect_node.dart
@@ -6,5 +6,17 @@ class ConnectNode {
     }
     return false;
   }
+
+  static void propeller(){
+    print("propeller");
+  }
+
+  static void random(){
+    print("random");
+  }
+
+  static void pet(){
+    print("pet");
+  }
 }
 // Criar enum para efeitos
